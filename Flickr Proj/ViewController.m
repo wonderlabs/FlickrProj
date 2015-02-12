e162f442900c33212d04f83e66a59800e08d36d7
Drasko Vucenovic <vucenod@gmail.com>
2015-02-12 00:44:49 -0500

    First Commit

--- Flickr Proj/ViewController.m
+++ Flickr Proj/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <FlickrKit.h>
 
 @interface ViewController ()
 
@@ -16,6 +17,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+   // [[FlickrKit sharedFlickrKit] initializeWithAPIKey:<#(NSString *)#> sharedSecret:<#(NSString *)#>];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
