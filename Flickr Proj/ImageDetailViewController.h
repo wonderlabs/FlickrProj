98c1ae51a9a889ed98c5df6c7dab78302c148e95
Drasko Vucenovic <vucenod@gmail.com>
2015-02-17 01:03:14 -0500

    - Added Image Detail View 	- Detail view displays image description, upload date, and Tags

---
+++ Flickr Proj/ImageDetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  ImageDetailViewController.h
+//  Flickr Proj
+//
+//  Created by Monica Brinkman on 2015-02-16.
+//  Copyright (c) 2015 Wonderlabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImageDetailViewController : UITableViewController
+
+@property (nonatomic, strong) NSString *imageId;
+@property (nonatomic, strong) UIImage *image;
+@property (strong, nonatomic) IBOutlet UITableView *myTableView;
+
+@end
